003c52122d3383b988cddc960db92ea9f3bc77b0
Muhammad uqba <uqba.waseem@xcorebit.com>
2023-11-22 11:03:32 +0500

    remove extra apis

--- lib/telnyx/wireguard_interface.rb
+++ lib/telnyx/wireguard_interface.rb
@@ -4,7 +4,6 @@ module Telnyx
   class WireguardInterface < APIResource
     extend Telnyx::APIOperations::List
     extend Telnyx::APIOperations::Create
-    extend Telnyx::APIOperations::Save
     extend Telnyx::APIOperations::Delete
 
     OBJECT_NAME = "wireguard_interface".freeze
